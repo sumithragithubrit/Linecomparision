7ce71d8949fb4bcde31fea3dfc08a1ac2fddeed8
sumi <sumipavib1302@gmail.com>
2022-11-23 14:03:10 +0530

    Length of Line

--- LineComparision.java
+++ LineComparision.java
@@ -1,9 +1,15 @@
 package linecomparision;
-
+import static java.lang.Math.sqrt;
 public class LineComparision {
+	 static int x1 = 6;
+	    static int x2 = 8;
+	    static int y1 = 2;
+	    static int y2 = 9;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program");
+		double LengthOfLine = sqrt( (x2 - x1) ^ 2 + (y2- y1) ^ 2);
+        System.out.println("Length of line is "+LengthOfLine );
 
 	}
 
